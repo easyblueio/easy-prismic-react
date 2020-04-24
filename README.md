86f1a550ace064c831c4e513cda8aca164d17c44
Maxime Bertonnier <m.bertonnier@gmail.com>
2020-04-24 12:54:55 +0200

    :white_check_mark: setup coverage badges

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## easy-prismic-react
 
+| Statements | Branches | Functions | Lines |
+| -----------|----------|-----------|-------|
+| ![Statements](https://img.shields.io/badge/Coverage-79.12%25-red.svg "Make me better!") | ![Branches](https://img.shields.io/badge/Coverage-61.63%25-red.svg "Make me better!") | ![Functions](https://img.shields.io/badge/Coverage-89.47%25-yellow.svg "Make me better!") | ![Lines](https://img.shields.io/badge/Coverage-79.12%25-red.svg "Make me better!") |
--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,35 @@
 module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "ejs",
-    "json"
+    'ts',
+    'tsx',
+    'js',
+    'ejs',
+    'json'
   ],
   moduleDirectories: [
-    "node_modules"
+    'node_modules'
   ],
-  "modulePaths": [
-    "<rootDir>"
+  'modulePaths': [
+    '<rootDir>'
   ],
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest'
   },
   testMatch: [
-    "**/*.(test|spec).(ts|tsx)"
+    '**/*.(test|spec).(ts|tsx)'
   ],
   globals: {},
   coveragePathIgnorePatterns: [
-    "/node_modules/"
+    '/node_modules/'
   ],
   coverageReporters: [
-    "json",
-    "lcov",
-    "text",
-    "text-summary"
+    'json-summary',
+    'text',
+    'lcov',
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/mocks.js",
-    "\\.(css|less|scss)$": "<rootDir>/__mocks__/mocks.js"
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/mocks.js',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/mocks.js'
   },
 };
